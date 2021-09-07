d963fcae3ae18f3110a64b8b7cf44c952df06de1
SAIRA <sarasheth60@gmail.com>
2021-09-07 21:57:59 +0530

    Blood Donor Receiver App

---
+++ Blood-App-master/app/src/main/java/com/ayush/bloodapp/Activities/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.ayush.bloodapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.ayush.bloodapp.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+                finish();
+            }
+        },2500);
+    }
+}
